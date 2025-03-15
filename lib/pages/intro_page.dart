3b66044223fa43026edb1890bbfcf66904394c53
Md. Hasibul Hasan <abeer.rocky@gmail.com>
2025-03-15 11:43:55 +0600

    get started page UI done

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -5,6 +5,51 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children:
+        [
+        //logo
+        Padding(
+          padding: const EdgeInsets.only(left: 80,right: 80,bottom: 40,top: 160),
+          child: Image.asset('assets/images/avocado.png'),
+        ),
+
+
+        //we deliver groceries  at your desktop
+        const Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Text('we deliver groceries  at your desktop',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+          ),),
+        ),
+
+        //fresh item everyday
+        Text('Fresh item every day'),
+
+
+        const Spacer(),
+        //get started button
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.deepPurple,
+            borderRadius: BorderRadius.circular(12)
+        
+          ),
+          padding: const EdgeInsets.all(24),
+          child: Text('Get Started',
+          style: TextStyle(
+              color: Colors.white70,
+          ),),
+        ),
+
+        const Spacer(),
+
+
+      ],),
+    );
   }
 }
